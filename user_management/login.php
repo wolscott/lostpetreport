94dc27198fef98491dbf13bf216b940577df555d
wolscott <scottwolcott@gmail.com>
2018-01-08 15:25:50 -0600

    update login system with prepared statements

--- user_management/login.php
+++ user_management/login.php
@@ -1,96 +1,53 @@
 <?php
 session_start();
 
-/*
-if(isset($_SESSION['username'])){
-	header("Location: /reRaven/index.php");
-}*/
 include_once '../config/db.php';
 
-
-
-if(isset($_POST['login'])){
 	$connection = connect();
-	$rawUsername = $_POST['username'];
-	$rawPassword = $_POST['password'];
-	$username = mysqli_real_escape_string($connection, $rawUsername);
-	$password = mysqli_real_escape_string($connection, $rawPassword);
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
 	$valid = true;
 
-	if(strcmp($rawUsername, $username) != 0 || strcmp($username, "") == 0) {
-		echo "bad username. try better";
-		$_SESSION['message'] = "bad username! Please try again.";
-		//header("Location: /reRaven/badUsername.php");
-		echo "rawusername: " . $rawUsername . ", clean username: " . $username;
+	if(strcmp($username, "") == 0 || strcmp($password, "") == 0) {
+		$_SESSION['message'] = "Bad username or password. Please try again.";
+
 		$valid = false;
 	}
-	if(strcmp($rawPassword, $password) != 0){
-		$_SESSION['message'] = "bad password! Please try again.";
-		//header("Location: /reRaven/badUsername.php");
-		echo "bad password, do a good job!";
-		$valid = false;
-	}
-
-
 
 	if($valid){
 		$newUser = true;
 		
-		$qry = "SELECT * FROM user WHERE username='$username'";
+		$qry = $connection->prepare("SELECT UserName, PasswordHash FROM user WHERE UserName=?");
 		
-		$result = $connection->query($qry);
-		
-		if($result->num_rows == 1){
-			$row = $result->fetch_assoc();
-			if(password_verify($password, $row['PasswordHash'])){
-				$_SESSION['username'] = $username;
-				header("Location: index.php");	
-			} else {
-				$_SESSION['message'] = "incorrect password! Please try again.";
-				//header("Location: /reRaven/badUsername.php");
-			}
-		}/*
-		while($row = $result->fetch_assoc()){
-			//echo "\nthere is a row";
-			echo $row['UserName'];
-		}*/
-/*
-		$qry = "INSERT INTO user(UserName, PasswordHash) VALUES ('" . $username . "', '" . password_hash($password, PASSWORD_DEFAULT) . "')";
-		echo $qry;
-		if(mysqli_query($connection, $qry)){
-			$_SESSION['message'] = "New User Created! (good job!)";
-			//header("Location: /reRaven/badUsername.php");
-			echo "u win";
-			//header("Location: /reRaven/" . $_SESSION['location']);	
-			header("Location: ../public_html/index.php");	
-		} else {
-			echo "u fail. ur db fails.";
-		} 
-*/
-	} else {
-		echo "ur dum";
-	}
-mysqli_close($connection);
+        $qry->bind_param('s', $username);
+        
+        try {
+            $qry->execute();
+        
+            $qry->store_result();
+            
+            $qry->bind_result($username, $passwordHash);
+
+            if($query->num_rows == 1){
+                $qry->fetch();
+    
+                if(password_verify($password, $passwordHash)){
+                    $_SESSION['username'] = $username;
+                    header("Location: index.php");	
+                } else {
+                    $_SESSION['message'] = "Login information is incorrect. Please try again.";
+                }
+            }
+        }
+        catch(PDOException $e){
+            echo "Error: " . $e->getMessage();
+        }
+        $qry->close();
+    }
+
+    mysqli_close($connection);
 }
 
-
 ?>
-<!--
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Login</title>
-	</head>
-	<body>
-		<div>
-			<form action="index.php" method="post" role="form">
-				<label for="username">username</label>
-				<input id="username" type="text" name="username">
-				<label for="password">password</label>
-				<input id="password" type="text" name="password">
-				<input id="loginButton" type="submit" name="login" value="login">
-			</form>
-		</div>
-	</body>
-</html>-->
+
