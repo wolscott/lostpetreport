8b19aa649f397e2ffcdbfbd5b6ad90da611c9f6d
wolscott <scottwolcott@gmail.com>
2018-01-09 02:38:08 -0600

    fixed duplicate name check

--- user_management/register.php
+++ user_management/register.php
@@ -33,72 +33,74 @@ if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['regcode'
         if($qry->num_rows == 1){
             /* username already exists */
             $qry->close();
-            header("Location: ../public_html/registration_page.php");
         }
-    }
-    catch(Exception $e){
-        echo "Error: " . $e->getMessage();
-    }
-    /* first check if the regcode is valid 
-     */
-    $qry = "SELECT RegCode FROM regcode WHERE RegCode=?";
-    if(!($qry = $connection->prepare($qry))){
-        echo "Prepare failed: (" . $connection->errno . ") " . $connection->error;
-    }
-    $qry->bind_param('s', $regcode);
+        else{
     
-    try {
-        $qry->execute();
-
-        $qry->store_result();
-
-        if($qry->num_rows >= 1){
-            /* then the regcode is valid. insert the inactive user and then delete the regcode */
-            $qry->close();
-            
-            $stmt = "INSERT INTO user (UserName, Email, CreationDate, PasswordHash, RegistrationCode, ActiveStatus)
-                     VALUES (?, ?, NOW(), ?, ?, ?)";
-    
-            if(!($stmt = $connection->prepare($stmt))){
+            /* first check if the regcode is valid 
+             */
+            $qry = "SELECT RegCode FROM regcode WHERE RegCode=?";
+            if(!($qry = $connection->prepare($qry))){
                 echo "Prepare failed: (" . $connection->errno . ") " . $connection->error;
             }
-            
-            $hashword = password_hash($password, PASSWORD_DEFAULT);
-            $activeStatus = 0;
-            
-            $stmt->bind_param('ssssi', $username, $email, $hashword, $regcode, $activeStatus);
-            
+            $qry->bind_param('s', $regcode);
+
             try {
-                $stmt->execute();
-                include_once 'send_email.php';
-                /* the email has been sent, delete the regcode */
-                $delete = "DELETE FROM regcode WHERE RegCode=?";
-                
-                if(!($delete = $connection->prepare($delete))){
-                    echo "Prepare failed: (" . $connection->errno . ") " . $connection->error;
+                $qry->execute();
+
+                $qry->store_result();
+
+                if($qry->num_rows >= 1){
+                    /* then the regcode is valid. insert the inactive user and then delete the regcode */
+                    $qry->close();
+
+                    $stmt = "INSERT INTO user (UserName, Email, CreationDate, PasswordHash, RegistrationCode, ActiveStatus)
+                             VALUES (?, ?, NOW(), ?, ?, ?)";
+
+                    if(!($stmt = $connection->prepare($stmt))){
+                        echo "Prepare failed: (" . $connection->errno . ") " . $connection->error;
+                    }
+
+                    $hashword = password_hash($password, PASSWORD_DEFAULT);
+                    $activeStatus = 0;
+
+                    $stmt->bind_param('ssssi', $username, $email, $hashword, $regcode, $activeStatus);
+
+                    try {
+                        $stmt->execute();
+                        include_once 'send_email.php';
+                        /* the email has been sent, delete the regcode */
+                        $delete = "DELETE FROM regcode WHERE RegCode=?";
+
+                        if(!($delete = $connection->prepare($delete))){
+                            echo "Prepare failed: (" . $connection->errno . ") " . $connection->error;
+                        }
+                        $delete->bind_param('s', $regcode);
+                        try {
+                            $delete->execute();
+                        }
+                        catch(Exception $e){
+                            echo "Error: " . $e->getMessage();
+                        }
+
+                        $delete->close();
+                    }
+                    catch(Exception $e){
+                        echo "Error: " . $e->getMessage();
+                    }
+
+                    $stmt->close();
+
                 }
-                $delete->bind_param('s', $regcode);
-                try {
-                    $delete->execute();
+                else {
+                    /* registration code is not valid */
+                    echo "<h2>Invalid Registration Code</h2>";
+                    echo "This code has been user or has expired. Contact your administrator.";
+
                 }
-                catch(Exception $e){
-                    echo "Error: " . $e->getMessage();
-                }
-                
-                $delete->close();
             }
             catch(Exception $e){
                 echo "Error: " . $e->getMessage();
             }
-            
-            $stmt->close();
-
-        }
-        else {
-            /* registration code is not valid */
-            echo "<h2>Invalid Registration Code</h2>";
-            echo "This code has been user or has expired. Contact your administrator.";
-            
         }
     }
     catch(Exception $e){
